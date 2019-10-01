dd9a840a9fd1eb06acf24a24290469c999b53cd1
Heorhii Siburov <g.siburov@gmail.com>
2019-10-01 12:34:38 -0700

    GET all posts from db - working

--- routes/hubs-router.js
+++ routes/hubs-router.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+const dataBase = require('../data/db.js');
 
+router.get('/', (req, res) => {
 
+    dataBase.find()
+    .then(posts => {
+        res.send(posts);
+    })
+    .catch(error => {
+        res.status(500).send({ error: "The users information could not be retrieved." })
+    });
 
+});
 
 // export
 module.exports = router;
