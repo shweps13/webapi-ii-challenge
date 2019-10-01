55895701eb8aabf2380ab88600886d3efe1cdebf
Heorhii Siburov <g.siburov@gmail.com>
2019-10-01 13:16:23 -0700

    GET by ID working

--- routes/hubs-router.js
+++ routes/hubs-router.js
@@ -11,11 +11,26 @@ router.post('/', (req, res) => {
     console.log('dataBase', dbData)
 
     dataBase.insert(dbData)
-    .then(user => {
-        res.status(201).json(user); 
+    .then(response => {
+        res.status(201).json(response); 
     })
     .catch(error => {
         res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }); 
+
+})
+
+// POST with ID post to database <== still need work
+router.post('/:id/comments', (req, res) => {
+    const dbData = req.body;
+    console.log('dataBase', dbData)
+
+    dataBase.insertComment(dbData)
+    .then(response => {
+        res.status(201).json(response); 
+    })
+    .catch(error => {
+        res.status(500).json({ error: "There was an error while saving the comment to the database" })
     }); 
 
 })
@@ -26,6 +41,21 @@ router.get('/', (req, res) => {
     dataBase.find()
     .then(posts => {
         res.send(posts);
+    })
+    .catch(error => {
+        res.status(500).send({ error: "The users information could not be retrieved." })
+    });
+
+});
+
+// GET post by ID
+router.get('/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    dataBase.findById(id)
+    .then(response => {
+        res.status(200).json(response)
     })
     .catch(error => {
         res.status(500).send({ error: "The users information could not be retrieved." })
