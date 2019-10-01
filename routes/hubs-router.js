cca8458c1b0d42e8de04ad29457c9d876fd8114c
Heorhii Siburov <g.siburov@gmail.com>
2019-10-01 12:57:00 -0700

    POST working

--- routes/hubs-router.js
+++ routes/hubs-router.js
@@ -4,6 +4,23 @@ const router = express.Router();
 
 const dataBase = require('../data/db.js');
 
+
+// POST post to database
+router.post('/', (req, res) => {
+    const dbData = req.body;
+    console.log('dataBase', dbData)
+
+    dataBase.insert(dbData)
+    .then(user => {
+        res.status(201).json(user); 
+    })
+    .catch(error => {
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }); 
+
+})
+
+// GET all posts
 router.get('/', (req, res) => {
 
     dataBase.find()
